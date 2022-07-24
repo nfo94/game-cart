4aed9a83d17da9c08d7f2071f3b06fe3ef72d7c1
Natália Oliveira <nataliaferreiraoliveira1994@gmail.com>
2022-07-24 17:40:44 -0300

    refactor: adjust StoreItem component

--- src/components/StoreItem.tsx
+++ src/components/StoreItem.tsx
@@ -21,11 +21,17 @@ export function StoreItem({ id, name, price, imgUrl }: StoreItemProps) {
 
   return (
     <Card className="h-100">
-      <Card.Img variant="top" src={imgUrl} />
+      <Card.Img
+        variant="top"
+        src={imgUrl}
+        height="250px"
+        className="mt-4"
+        style={{ objectFit: "contain" }}
+      />
 
       <Card.Body className="">
         <Card.Title className="d-flex justify-content-between align-items-baseline mb-4">
-          <span className="fs-3">{name}</span>
+          <span className="fs-4">{name}</span>
           <span className="ms-2 text-muted">{formatCurrency(price)}</span>
         </Card.Title>
 
